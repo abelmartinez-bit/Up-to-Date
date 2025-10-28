5e1077f9884b12b543761446f498ad45da5c3365
Abel <martinezreyesabel.70@gmail.com>
2025-10-27 17:30:54 -0700

    Switch to GNews API which supports browser CORS requests

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,36 +1,25 @@
 import axios from 'axios';
 import { API_CONFIG } from '../config/constants';
 
-// Use CORS proxy to bypass News API browser restrictions
-const CORS_PROXY = 'https://corsproxy.io/?';
-
+// GNews API supports CORS - no proxy needed!
 const api = axios.create({
+  baseURL: API_CONFIG.BASE_URL,
   timeout: 10000,
 });
-
-/**
- * Build full URL with CORS proxy
- */
-const buildProxyUrl = (endpoint, params) => {
-  const url = new URL(endpoint, API_CONFIG.BASE_URL);
-  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-  return CORS_PROXY + encodeURIComponent(url.toString());
-};
 
 /**
  * Fetch news articles by category
  */
 export const fetchNewsByCategory = async (categoryQuery, page = 1) => {
   try {
-    const url = buildProxyUrl('/everything', {
-      q: categoryQuery,
-      apiKey: API_CONFIG.API_KEY,
-      pageSize: API_CONFIG.PAGE_SIZE,
-      page: page,
-      sortBy: 'publishedAt',
-      language: 'en',
+    const response = await api.get('/search', {
+      params: {
+        q: categoryQuery,
+        apikey: API_CONFIG.API_KEY,
+        max: API_CONFIG.PAGE_SIZE,
+        lang: 'en',
+      },
     });
-    const response = await api.get(url);
     return response.data;
   } catch (error) {
     throw handleApiError(error);
@@ -44,18 +33,16 @@ export const searchNews = async (query, fromDate = null) => {
   try {
     const params = {
       q: query,
-      apiKey: API_CONFIG.API_KEY,
-      pageSize: API_CONFIG.PAGE_SIZE,
-      sortBy: 'publishedAt',
-      language: 'en',
+      apikey: API_CONFIG.API_KEY,
+      max: API_CONFIG.PAGE_SIZE,
+      lang: 'en',
     };
 
     if (fromDate) {
-      params.from = fromDate;
+      params.from = fromDate + 'T00:00:00Z';
     }
 
-    const url = buildProxyUrl('/everything', params);
-    const response = await api.get(url);
+    const response = await api.get('/search', { params });
     return response.data;
   } catch (error) {
     throw handleApiError(error);
@@ -67,12 +54,13 @@ export const searchNews = async (query, fromDate = null) => {
  */
 export const fetchTopHeadlines = async (country = 'us') => {
   try {
-    const url = buildProxyUrl('/top-headlines', {
-      country: country,
-      apiKey: API_CONFIG.API_KEY,
-      pageSize: API_CONFIG.PAGE_SIZE,
+    const response = await api.get('/top-headlines', {
+      params: {
+        country: country,
+        apikey: API_CONFIG.API_KEY,
+        max: API_CONFIG.PAGE_SIZE,
+      },
     });
-    const response = await api.get(url);
     return response.data;
   } catch (error) {
     throw handleApiError(error);
